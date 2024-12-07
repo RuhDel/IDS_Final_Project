023a02e9603e187785ca70c8e8fd75bfd9567533
Uriel Pena <115737277+blnkin@users.noreply.github.com>
2024-12-06 19:44:15 -0600

    Update combine_csv.py

--- scripts/combine_csv.py
+++ scripts/combine_csv.py
@@ -13,14 +13,12 @@ def combine_csv_files(input_dir, output_file):
         combined_df = pd.concat([combined_df, df], ignore_index=True)
         print(f"Combined {file} into the main DataFrame.")
 
-    # Optionally, reset index
     combined_df.reset_index(drop=True, inplace=True)
 
-    # Save the combined DataFrame to a CSV file
     combined_df.to_csv(output_file, index=False)
     print(f"All CSV files combined into {output_file}.")
 
 if __name__ == "__main__":
-    input_directory = './data/'        # Directory containing the 8 CSV files
-    output_csv = './data/combined.csv' # Output combined CSV file
+    input_directory = './data/'
+    output_csv = './data/combined.csv'
     combine_csv_files(input_directory, output_csv)
